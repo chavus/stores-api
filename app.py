a5f89c8a22762557c130c09c9c5f3e8b7bc62d6e
ChavaJN <chavajn@ChavaMac.local>
2020-02-06 18:23:29 -0600

    Fix typo import os

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environment.get('DATABASE_URL', 'sqlite:///data.db')  # if no DATABASE_URL is configured, second value is taken *default
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')  # if no DATABASE_URL is configured, second value is taken *default
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'chava'
 api = Api(app)
